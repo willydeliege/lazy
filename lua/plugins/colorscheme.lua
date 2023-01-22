91243cc14d31883626921c2d3e6917f95373a90f
Frédéric Willem <frederic.willem@gmail.com>
2023-01-22 10:30:53 +0100

    add tokyonight background dark

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,7 +6,13 @@ return {
     priority = 1000,
     config = function()
       -- Lua
-      require("tokyonight").setup({})
+      require("tokyonight").setup({
+        style = "night",
+        on_colors = function(colors)
+          colors.bg = "#000000"
+          colors.bg_dark = "#000000"
+        end,
+      })
       -- Lua
       vim.cmd([[colorscheme tokyonight-night]])
     end,
